670fe9dbf2e66cf93bb02b5f56c263949f6e044c
CaueGrassi <128317143+CaueGrassi7@users.noreply.github.com>
2024-03-29 17:18:52 -0300

    Update grafos.h

--- Programa/grafos.h
+++ Programa/grafos.h
@@ -5,30 +5,32 @@ typedef int TipoValorVertice;
 typedef int TipoPeso;
 
 typedef struct TipoItem {
-    TipoValorVertice vertice;
-    TipoPeso peso;
+  TipoValorVertice vertice;
+  TipoPeso peso;
 } TipoItem;
 
 struct TipoCelula {
-    TipoItem item;
-    struct TipoCelula *prox;
+  TipoItem item;
+  struct TipoCelula *prox;
 };
 
 typedef struct TipoCelula *TipoApontador;
 
 typedef struct TipoLista {
-    TipoApontador primeiro, ultimo;
+  TipoApontador primeiro, ultimo;
 } TipoLista;
 
 typedef struct TipoGrafo {
-    TipoLista adj[MAXNUMVERTICES];
-    int numVertices;
-    int numArestas;
+  TipoLista adj[MAXNUMVERTICES];
+  int numVertices;
+  int numArestas;
 } TipoGrafo;
 
 void FGVazio(TipoGrafo *grafo);
-void InsereAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso, TipoGrafo *grafo);
+void InsereAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso,
+                  TipoGrafo *grafo);
 int ExisteAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoGrafo *grafo);
-void RetiraAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso, TipoGrafo *grafo);
+void RetiraAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso,
+                  TipoGrafo *grafo);
 void ImprimeGrafo(TipoGrafo *grafo);
 void LiberaGrafo(TipoGrafo *grafo);
