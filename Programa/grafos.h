5b851e67b13ff6356e02071938c0287a11da2c8d
Pedro Trevisan <104599997+pedrootrevisan@users.noreply.github.com>
2024-04-06 17:47:26 -0300

    commit grafos

--- Programa/grafos.h
+++ Programa/grafos.h
@@ -1,3 +1,6 @@
+#ifndef GRAFOS_H_INCLUDED
+#define GRAFOS_H_INCLUDED
+
 #define MAXNUMVERTICES 100
 #define MAXNUMARESTAS 4500
 
@@ -7,6 +10,8 @@ typedef int TipoPeso;
 typedef struct TipoItem {
   TipoValorVertice vertice;
   TipoPeso peso;
+  int ehSaida;     // 1 para saída e 0 para padrão
+  int ehSumidouro; // 1 para sumidouro e 0 para padrão
 } TipoItem;
 
 struct TipoCelula {
@@ -27,10 +32,15 @@ typedef struct TipoGrafo {
 } TipoGrafo;
 
 void FGVazio(TipoGrafo *grafo);
-void InsereAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso,
-                  TipoGrafo *grafo);
-int ExisteAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoGrafo *grafo);
-void RetiraAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso,
-                  TipoGrafo *grafo);
+void InsereAresta(TipoValorVertice v1, TipoValorVertice v2, TipoPeso peso,
+                  TipoGrafo *grafo, int ehSaida, int ehSumidouro);
+int ExisteAresta(TipoValorVertice v1, TipoValorVertice v2, TipoGrafo *grafo);
+void RetiraAresta(TipoValorVertice v1, TipoValorVertice v2, TipoGrafo *grafo);
 void ImprimeGrafo(TipoGrafo *grafo);
 void LiberaGrafo(TipoGrafo *grafo);
+void exibirOpcoes(TipoGrafo *grafo, TipoValorVertice verticeAtual);
+TipoApontador encontrarVertice(TipoGrafo *grafo, TipoValorVertice verticeAtual,
+                               int escolha);
+void percorrerGrafo(TipoGrafo *grafo, TipoValorVertice verticeInicial);
+
+#endif // GRAFOS_H_INCLUDED
--- Programa/grafosImp.c
+++ Programa/grafosImp.c
@@ -2,57 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXNUMVERTICES 100
-#define MAXNUMARESTAS 4500
-
-typedef int TipoValorVertice;
-typedef int TipoPeso;
-
 void FGVazio(TipoGrafo *grafo) {
   int i;
-  for (i = 0; i < grafo->numVertices; i++) {
+  for (i = 0; i < MAXNUMVERTICES; i++) {
     grafo->adj[i].primeiro = (TipoApontador)malloc(sizeof(struct TipoCelula));
     grafo->adj[i].ultimo = grafo->adj[i].primeiro;
     grafo->adj[i].primeiro->prox = NULL;
   }
+  grafo->numVertices = 0;
+  grafo->numArestas = 0;
 }
 
-void InsereAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso,
-                  TipoGrafo *grafo) {
-  TipoApontador novoNo;
-  novoNo = (TipoApontador)malloc(sizeof(struct TipoCelula));
-  novoNo->item.vertice = *v2;
-  novoNo->item.peso = *peso;
+void InsereAresta(TipoValorVertice v1, TipoValorVertice v2, TipoPeso peso,
+                  TipoGrafo *grafo, int ehSaida, int ehSumidouro) {
+  TipoApontador novoNo = (TipoApontador)malloc(sizeof(struct TipoCelula));
+  novoNo->item.vertice = v2;
+  novoNo->item.peso = peso;
+  novoNo->item.ehSaida = ehSaida;
+  novoNo->item.ehSumidouro = ehSumidouro;
   novoNo->prox = NULL;
-  grafo->adj[*v1].ultimo->prox = novoNo;
-  grafo->adj[*v1].ultimo = novoNo;
+  grafo->adj[v1].ultimo->prox = novoNo;
+  grafo->adj[v1].ultimo = novoNo;
   grafo->numArestas++;
 }
 
-int ExisteAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoGrafo *grafo) {
-  TipoApontador aux;
-  aux = grafo->adj[*v1].primeiro->prox;
-  while (aux != NULL && aux->item.vertice != *v2) {
+int ExisteAresta(TipoValorVertice v1, TipoValorVertice v2, TipoGrafo *grafo) {
+  TipoApontador aux = grafo->adj[v1].primeiro->prox;
+  while (aux != NULL) {
+    if (aux->item.vertice == v2) {
+      return 1;
+    }
     aux = aux->prox;
   }
-  if (aux == NULL) {
-    return 0;
-  } else {
-    return 1;
-  }
+  return 0;
 }
 
-void RetiraAresta(TipoValorVertice *v1, TipoValorVertice *v2, TipoPeso *peso,
-                  TipoGrafo *grafo) {
+void RetiraAresta(TipoValorVertice v1, TipoValorVertice v2, TipoGrafo *grafo) {
   TipoApontador aux, ant;
-  ant = grafo->adj[*v1].primeiro;
-  aux = grafo->adj[*v1].primeiro->prox;
-  while (aux != NULL && aux->item.vertice != *v2) {
+  ant = grafo->adj[v1].primeiro;
+  aux = grafo->adj[v1].primeiro->prox;
+  while (aux != NULL && aux->item.vertice != v2) {
     ant = aux;
     aux = aux->prox;
   }
   if (aux != NULL) {
     ant->prox = aux->prox;
+    if (grafo->adj[v1].ultimo == aux) {
+      grafo->adj[v1].ultimo = ant;
+    }
     free(aux);
   }
 }
@@ -83,4 +80,86 @@ void LiberaGrafo(TipoGrafo *grafo) {
     }
   }
   grafo->numVertices = 0;
+}
+
+TipoApontador encontrarVertice(TipoGrafo *grafo, TipoValorVertice verticeAtual,
+                               int escolha) {
+  if (escolha < 1) {
+    printf("Opção inválida. Tente novamente.\n");
+    return NULL;
+  }
+
+  TipoApontador aux = grafo->adj[verticeAtual].primeiro->prox;
+  int contador = 1;
+
+  while (aux != NULL && contador < escolha) {
+    aux = aux->prox;
+    contador++;
+  }
+
+  if (aux == NULL || contador != escolha) {
+    printf("Opção inválida. Tente novamente.\n");
+    return NULL;
+  }
+
+  return aux;
+}
+
+void exibirOpcoes(TipoGrafo *grafo, TipoValorVertice verticeAtual) {
+  int contador = 1;
+  printf("\nOpções de movimento a partir da sala %d:\n", verticeAtual);
+
+  TipoApontador adjacente = grafo->adj[verticeAtual].primeiro->prox;
+  if (!adjacente) {
+    printf("Sem saídas disponíveis.\n");
+    return;
+  }
+
+  while (adjacente) {
+    printf("%d. Sala %d", contador, adjacente->item.vertice);
+    if (adjacente->item.ehSaida)
+      printf(" - Saída");
+    if (adjacente->item.ehSumidouro)
+      printf(" - Sumidouro");
+    printf("\n");
+    adjacente = adjacente->prox;
+    contador++;
+  }
+  printf("Escolha uma opção ou digite -1 para sair.\n");
+}
+
+void percorrerGrafo(TipoGrafo *grafo, TipoValorVertice verticeInicial) {
+  TipoValorVertice verticeAtual = verticeInicial;
+  printf("Explorando o labirinto...\n");
+
+  while (1) {
+    exibirOpcoes(grafo, verticeAtual);
+    printf("Sua escolha: ");
+    int escolha;
+    if (scanf("%d", &escolha) != 1) {
+      printf("Entrada inválida.\n");
+      while (getchar() != '\n')
+        ; // Limpa o buffer
+      continue;
+    }
+
+    if (escolha == -1) {
+      printf("Exploração encerrada.\n");
+      break;
+    }
+
+    TipoApontador proximo = encontrarVertice(grafo, verticeAtual, escolha);
+    if (!proximo)
+      continue;
+
+    verticeAtual = proximo->item.vertice;
+    if (proximo->item.ehSumidouro) {
+      printf("Caiu em um sumidouro! Fim do jogo.\n");
+      break;
+    }
+    if (proximo->item.ehSaida) {
+      printf("Saída encontrada! Fim da exploração.\n");
+      break;
+    }
+  }
 }
